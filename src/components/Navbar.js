d5ca386a54446fede61320dbba1e81fd3e67640d
Gaëlle Boucher <gaelleboucher23@gmail.com>
2024-10-28 16:05:29 +0100

    navbar et css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,12 +7,12 @@ function Navbar() {
         <div className="navbar-container"> 
             <img src={Logo} alt="Logo GB WebAssist" /> 
             <h1>GB WebAssist</h1>
-            <div>
-                <div><a href="#accueil" class="heberg">Accueil</a></div>
-                <div><a href="#services" class="acti">Services</a></div>
-                <div><a href="#tarifs" class="inscription">Tarifs</a></div>
-                <div><a href="#contact" class="inscription">Contact</a></div>
-            </div>
+            <ul>
+                <li>Accueil</li>
+                <li>Services</li>
+                <li>Tarifs</li>
+                <li>Contact</li>
+            </ul>
         </div>
     </nav>
     );
