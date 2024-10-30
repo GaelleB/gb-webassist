426f555bf98696e03715c55268052b5292cb5ab1
Gaëlle Boucher <gaelleboucher23@gmail.com>
2024-10-30 19:04:42 +0100

    tarifs

--- src/components/Tarifs.js
+++ src/components/Tarifs.js
@@ -6,30 +6,26 @@ function Tarifs() {
             <div className="container">
                 <h2>Tarifs</h2>
                 <table>
-                <thead>
-                    <tr>
-                    <th>Prestation</th>
-                    <th>Tarif</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>Création de site vitrine</td>
-                    <td>à partir de 500€</td>
-                    </tr>
-                    <tr>
-                    <td>Création de site e-commerce</td>
-                    <td>à partir de 1000€</td>
-                    </tr>
-                    <tr>
-                    <td>Dépannage informatique à domicile</td>
-                    <td>50€/heure</td>
-                    </tr>
-                    <tr>
-                    <td>Configuration d'ordinateur</td>
-                    <td>80€</td>
-                    </tr>
-                </tbody>
+                    <thead>
+                        <tr>
+                            <th>Prestation</th>
+                            <th>Tarif</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Création de site vitrine</td>
+                            <td>à partir de 600€</td>
+                        </tr>
+                        <tr>
+                            <td>Création de site e-commerce</td>
+                            <td>à partir de 1500€</td>
+                        </tr>
+                        <tr>
+                            <td>Dépannage informatique à domicile</td>
+                            <td>30€/heure</td>
+                        </tr>
+                    </tbody>
                 </table>
                 <p>
                 * Ces tarifs sont donnés à titre indicatif et peuvent varier en fonction de la complexité du projet.
